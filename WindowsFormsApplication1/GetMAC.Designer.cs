34e4f601dc75eaf7085617adff26a5c9c87ef036
bannov <social.bannov@gmail.com>
2017-04-05 18:13:26 +0200

    beta test version

---
+++ WindowsFormsApplication1/GetMAC.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class GetMAC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "localhost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter PC-Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GetMac";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(234, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MAC address has been copied to clipboard";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Enter the host Administrator login and password";
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameBox.Location = new System.Drawing.Point(16, 149);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(114, 22);
+            this.UserNameBox.TabIndex = 6;
+            this.UserNameBox.Text = "orbann.adm";
+            // 
+            // PassBox
+            // 
+            this.PassBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassBox.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.PassBox.Location = new System.Drawing.Point(19, 191);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(110, 22);
+            this.PassBox.TabIndex = 7;
+            this.PassBox.Text = "Enterthepass";
+            this.PassBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PassBox_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "oleksandr.bannov@gfk.com";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // GetMAC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 254);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GetMAC";
+            this.Text = "GetMAC";
+            this.Load += new System.EventHandler(this.GetMAC_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication1/GetMAC.cs
@@ -0,0 +1,120 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class GetMAC : Form
+    {
+        //  private String user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        private String user = Environment.UserName;
+        private String pass;
+        private String pcName;
+
+        private void GetMAC_Load(object sender, EventArgs e)
+        {
+            if (user == "Administrator" || user == "administrator" || user == "Admin" || user == "admin")
+            {
+                UserNameBox.Text = user;
+            }
+            else
+            {
+                UserNameBox.Text = user + ".adm";
+            }
+        }
+
+        public GetMAC()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+             pcName = textBox1.Text;
+            // do job
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "getmac",
+                    Arguments = "/fo table /v",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+
+             
+            };
+
+                user = UserNameBox.Text;
+                 pass = PassBox.Text;
+
+
+            if (pcName != "localhost" && pcName != null) {
+                
+                proc.StartInfo.Arguments = "/s " + pcName + " /U "+user+" /P "+pass+" /fo list /v";
+
+               // label5.Text = proc.StartInfo.Arguments;
+            }
+            proc.Start();
+            string error = proc.StandardError.ReadToEnd();
+            proc.WaitForExit();
+            if (error != null)
+                label2.Text = error;
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                
+                if (line.Contains("LAN-Verbindung"))
+                {
+                    
+                      String[] tmpString = line.Split(' ');
+                    if (tmpString[4] == "GB")
+                    {
+                        label2.Text = tmpString[0] + " " + tmpString[4] + " " + tmpString[5];
+                        System.Windows.Forms.Clipboard.SetText(tmpString[5]);
+                        label3.Visible = true;
+                    }
+                    else {
+                        label2.Text = tmpString[0] + " " + tmpString[4];
+                        System.Windows.Forms.Clipboard.SetText(tmpString[4]);
+                        label3.Visible = true;
+
+                    }
+
+                }
+            
+                // do something with line}
+            }
+
+
+        } // end button
+
+        private void PassBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            PassBox.Text = "";
+            PassBox.ForeColor = Color.Black; 
+            PassBox.PasswordChar = '*';
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:oleksandr.bannov@gfk.com");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
